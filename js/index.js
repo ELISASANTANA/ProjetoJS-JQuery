c840df535cbd3ea23f69edc446a780f2909d943f
ELISASANTANA <elisa.sagon24@gmail.com>
2021-10-27 03:21:57 -0300

    Alterações e responsividade em andamento

---
+++ js/index.js
@@ -0,0 +1,43 @@
+// chamei jquery pelo id sdministrativo par o evento mouseover
+$("#administrativo").mouseover(function(){
+    //qndo passar o mouse em cima da foto aparecea
+   // caixa de texto
+    $("[name=administrativo]").show(800)
+}).mouseout(function(){
+    // jquery hide esconde a caixa de texto em 500 ms
+    $("[name=administrativo]").hide(500)
+})
+//segue fazendo igual para todos membros da equipe 
+$("#arquitetura").mouseover(function(){
+    $("[name=arquitetura]").show(800)
+}).mouseout(function(){
+    $("[name=arquitetura]").hide(500)
+})
+
+$("#marketing").mouseover(function(){
+    $("[name=marketing]").show(800)
+}).mouseout(function(){
+    $("[name=marketing]").hide(500)
+})
+$("#comercial").mouseover(function(){
+    $("[name=comercial]").show(800)
+}).mouseout(function(){
+    $("[name=comercial]").hide(500)
+})
+
+
+
+
+//outra forma de fazer 
+// $("img").click(function() {
+    
+//     $(".texto").css("display","none")
+//     var nome = $(this).attr("id")
+//     $("p[name='"+ nome + "']").css("display","block")
+
+//     console.log(this);
+//     console.log(nome);
+    
+// });
+
+
